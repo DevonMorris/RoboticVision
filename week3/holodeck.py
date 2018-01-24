b0338fc810debb78dd3d636e53636e734fa82266
Devon Morris <devonmorris1992@gmail.com>
2018-01-23 17:02:25 -0700

    updated holodeck with filters in OpenCV

--- week3/holodeck.py
+++ week3/holodeck.py
@@ -3,11 +3,22 @@ import numpy as np
 import cv2
 import pygame as pg
 
+from enum import Enum
+
 from Holodeck import Holodeck, Agents
 from Holodeck.Environments import HolodeckEnvironment
 from Holodeck.Sensors import Sensors
 
-def filter_keys(keys, command):
+class Filter(Enum):
+    NO_FILTER = 0
+    CANNY = 1
+    SOBEL = 2
+    SOBELX = 3
+    SOBELY = 4
+    GAUSSIAN = 5
+    GRADIENT = 6
+
+def filter_keys(keys, command, filt):
     if keys[pg.K_a]:
         command[2] += -0.03
     if keys[pg.K_d]:
@@ -24,12 +35,43 @@ def filter_keys(keys, command):
         command[0] -= 0.01
     if keys[pg.K_RIGHT]:
         command[0] += 0.01
+    if keys[pg.K_0]:
+        filt = Filter.NO_FILTER
+    if keys[pg.K_1]:
+        filt = Filter.CANNY
+    if keys[pg.K_2]:
+        filt = Filter.SOBEL
+    if keys[pg.K_3]:
+        filt = Filter.SOBELX
+    if keys[pg.K_4]:
+        filt = Filter.SOBELY
+    if keys[pg.K_5]:
+        filt = Filter.GAUSSIAN
+    if keys[pg.K_6]:
+        filt = Filter.GRADIENT
 
-    return command
+    return command, filt
+
+def filter_image(frame, filt):
+    if filt == Filter.NO_FILTER:
+        pass
+    elif filt == Filter.CANNY:
+        frame = cv2.Canny(frame, 100, 300)
+    elif filt == Filter.SOBEL:
+        frame = cv2.Sobel(frame, cv2.CV_32F, 1, 1)
+    elif filt == Filter.SOBELX:
+        frame = cv2.Sobel(frame, cv2.CV_32F, 1, 0)
+    elif filt == Filter.SOBELY:
+        frame = cv2.Sobel(frame, cv2.CV_32F, 0, 1)
+    elif filt == Filter.GAUSSIAN:
+        frame = cv2.GaussianBlur(frame, (9,9), 2.0)
+    elif filt == Filter.GRADIENT:
+        frame = cv2.Laplacian(frame, cv2.CV_32F)
+    return frame
 
 def uav_holodeck():
     # Make holodeck environment
-    env = Holodeck.make("ConiferForest")
+    env = Holodeck.make("UrbanCity")
     env.reset()
 
     gameDisplay = pg.display.set_mode((512, 512))
@@ -40,7 +82,9 @@ def uav_holodeck():
     pg.event.pump()
     keys = pg.key.get_pressed()
     command = [0.0, 0.0, 0.0, 0.0]
-    command = filter_keys(keys, command)
+    filt = 0
+
+    command, filt = filter_keys(keys, command, filt)
 
     while (not keys[pg.K_ESCAPE]):
         state, reward, terminal, _ = env.step(command)
@@ -48,13 +92,16 @@ def uav_holodeck():
         # To access specific sensor data:
         frame = cv2.cvtColor(state[Sensors.PRIMARY_PLAYER_CAMERA], cv2.COLOR_BGRA2RGB)
         frame = np.rot90(frame)
+
+        frame = filter_image(frame, filt)
+
         frame = pg.surfarray.make_surface(frame)
         gameDisplay.blit(frame, (0,0))
         pg.display.update()
 
         pg.event.pump()
         keys = pg.key.get_pressed()
-        command = filter_keys(keys, command)
+        command, filt = filter_keys(keys, command, filt)
 
 if __name__ == "__main__":
     pg.init()
